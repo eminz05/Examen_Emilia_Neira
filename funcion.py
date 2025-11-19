253e4c0c270f2997190e591643185d53f7db7a79
eminz05 <emi.neira@duocuc.cl>
2025-11-19 18:36:16 -0300

    18:36

--- funcion.py
+++ funcion.py
@@ -1,15 +1,39 @@
+# #[título, autor, tipo, páginas, editorial, idioma]
+# catalogo = {
+#  'BK001': ['El Quijote', 'Miguel de Cervantes', 'físico', 1200, 'Editorial Planeta', 'español'],
+#  'BK002': ['1984', 'George Orwell', 'digital', 328, 'Penguin', 'inglés'],
+#  'BK003': ['Rayuela', 'Julio Cortázar', 'físico', 600, 'Sudamericana', 'español'],
+#  'BK004': ['Sapiens', 'Yuval Noah Harari', 'digital', 450, 'Debate', 'inglés'],
+ 
+# }
+
+# #[precio, unidades_disponibles]
+# inventario = {
+#  'BK001': [15990, 3],
+#  'BK002': [8990, 0],
+#  'BK003': [18990, 7],
+#  'BK004': [12500, 5],
+ 
+# }
+
 def stock_editorial(editorial):
     editorial=input("Ingrese el nombre de la editorial: ").strip()
     for valor in catalogo:
         catalogo=valor[4]["editorial"]
-        print(f"Stock editorial {catalogo[4]}")
+        editorial=catalogo[4]
+
+    for llave in inventario:
+        inventario=llave[0]["unidades"]
+    stock_editorial= stock_editorial[llave][0]=+1
+
+    print(f"Stock editorial {editorial} {stock_editorial}")
     return editorial
 
 def buscar_por_precio(p_min, p_max):
     while True:
         try:
-            p_min=int(input("Ingrese el precio mínimo: "))#.index()
-            p_max=int(input("Ingrese el precio máximo: "))#.index()
+            p_min=int(input("Ingrese el precio mínimo: ")).index()
+            p_max=int(input("Ingrese el precio máximo: ")).index()
         except ValueError:
             print("Debe ingresar números enteros!!")
 
@@ -24,7 +48,6 @@ def buscar_por_precio(p_min, p_max):
 
     return p_min,p_max
 
-        
 def actualizar_precio(codigo, nuevo_precio):
     codigo=input("Ingrese el código del libro: ").upper().strip()
     nuevo_precio=int(input("Ingrese el nuevo precio del libro: "))
@@ -36,3 +59,5 @@ def actualizar_precio(codigo, nuevo_precio):
     else:
         print("El libro no existe!!")
         return False
+
+    return codigo, nuevo_precio
